ca50c3a30cd87dc2b8833a176b22e88e6d8d7741
kmzn <kmzn777@gmail.com>
2014-02-17 10:33:57 +0000

    add class file for getting svn infomation

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
+using System.IO;
 
 namespace WindowsFormsApplication1
 {
@@ -63,35 +65,9 @@ namespace WindowsFormsApplication1
             sec++;
             Console.WriteLine(sec.ToString() + "秒経過...");
 
-            //Processオブジェクトを作成
-            System.Diagnostics.Process p = new System.Diagnostics.Process();
-
-            //ComSpec(cmd.exe)のパスを取得して、FileNameプロパティに指定
-            p.StartInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
-            //出力を読み取れるようにする
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.RedirectStandardOutput = true;
-            p.StartInfo.RedirectStandardInput = false;
-            //ウィンドウを表示しないようにする
-            p.StartInfo.CreateNoWindow = true;
-            //コマンドラインを指定（"/c"は実行後閉じるために必要）
-            p.StartInfo.Arguments = @"/c C:\\cygwin\\bin\\svnversion.exe ";
-
-            //起動
-            p.Start();
-
-            //出力を読み取る
-            string results = p.StandardOutput.ReadToEnd();
-
-            //プロセス終了まで待機する
-            //WaitForExitはReadToEndの後である必要がある
-            //(親プロセス、子プロセスでブロック防止のため)
-            p.WaitForExit();
-            p.Close();
-
-            //出力された結果を表示
-            Console.WriteLine(results);
-
+            SvnGetter svnGetter = new SvnGetter();
+            svnGetter.GetInfomation(url);
+            Console.WriteLine(svnGetter.GetRevisionNumber());
         }
     }
 }
---
+++ WindowsFormsApplication1/SvnGetter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+
+    
+class SvnGetter
+    {
+        private string svnInfomationXml = "";
+
+        public bool GetInfomation(string queryUrl)
+        {
+            //Processオブジェクトを作成
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+
+            //ComSpec(cmd.exe)のパスを取得して、FileNameプロパティに指定
+            p.StartInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
+            //出力を読み取れるようにする
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = false;
+            //ウィンドウを表示しないようにする
+            p.StartInfo.CreateNoWindow = true;
+            //コマンドラインを指定（"/c"は実行後閉じるために必要）
+            // svn info $url --xml
+            p.StartInfo.Arguments = "/c c:/\"Program Files (x86)\"/Subversion/bin/svn.exe info --xml " + queryUrl;//@"/c C:\\'Program Files (x86)'\\Subversion\\bin\\svnversion.exe ";
+
+            //起動
+            p.Start();
+
+            //出力を読み取る
+            svnInfomationXml = p.StandardOutput.ReadToEnd();
+
+            //プロセス終了まで待機する
+            //WaitForExitはReadToEndの後である必要がある
+            //(親プロセス、子プロセスでブロック防止のため)
+            p.WaitForExit();
+            p.Close();
+
+            return true;
+        }
+
+        public string GetRevisionNumber()
+        {
+            // Create an XmlReader
+            using (XmlReader reader = XmlReader.Create(new StringReader(svnInfomationXml)))
+            {
+                reader.ReadToFollowing("entry");
+                reader.MoveToNextAttribute();
+                reader.MoveToNextAttribute();
+                reader.MoveToNextAttribute();
+
+                return reader.Value;
+            }
+        }
+    }
+
